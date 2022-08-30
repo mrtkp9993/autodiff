6f9db532b72db26db7c122eae4c0745d2bb394a5
Murat Koptur <mkoptur3@gmail.com>
2022-08-30 14:25:50 +0300

    init

---
+++ autodiff_lib/autodiff_lib.cpp
@@ -0,0 +1,10 @@
+// autodiff_lib.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: This is an example of a library function
+void fnautodifflib()
+{
+}
