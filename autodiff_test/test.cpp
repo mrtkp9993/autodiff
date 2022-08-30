4e47fe47615c45aa6795302810ab7ac4d71a2303
Murat Koptur <mkoptur3@gmail.com>
2022-08-30 14:49:22 +0300

    transfer project

--- autodiff_test/test.cpp
+++ autodiff_test/test.cpp
@@ -1,6 +1,9 @@
 #include "pch.h"
+#include "../autodiff_lib/autodiff_lib.cpp"
 
 TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+	Dual x(5, 1);
+	Dual y(6);
+	Dual f = pow(x, 2) * y;
+	EXPECT_EQ(60, f.dual);
 }
